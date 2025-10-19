f77843ae8d647805b5a9dbfccbb6e67cb5bb47c4
Sivasuthan Sukumar <153247727+Sivasuthan9@users.noreply.github.com>
2025-10-19 22:40:07 +0530

    service is now correctly return DTO

--- patient-service/src/main/java/com/pm/patientservice/service/PatientService.java
+++ patient-service/src/main/java/com/pm/patientservice/service/PatientService.java
@@ -1,11 +1,14 @@
 package com.pm.patientservice.service;
 
 import com.pm.patientservice.dto.PatientResponseDTO;
+import com.pm.patientservice.mapper.PatientMapper;
 import com.pm.patientservice.model.Patient;
 import com.pm.patientservice.repository.PatientRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class PatientService {
@@ -17,5 +20,6 @@ public class PatientService {
 
     public List<PatientResponseDTO> getPatients() {
         List<Patient> patients = patientRepository.findAll();
+        return patients.stream().map(PatientMapper::toPatientDTO).toList();
     }
 }
